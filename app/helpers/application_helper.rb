0cf038f5198e6cdcf3a45e568851c0e73a35b5c1
latem999 <latem999@gmail.com>
2013-06-25 11:21:26 +0200

    Improve timestamp formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Formats a timestamp as dd mm yy hh:mm or Today, hh:mm if it's today's date. Well, something like this is probably already in rails ;]
+  def format_timestamp(timestamp)
+    if timestamp.today?
+      "Today, #{timestamp.strftime("%H:%M")}"
+    else
+      timestamp.strftime("%d %b. %Y %H:%M")
+    end
+  end
 end
